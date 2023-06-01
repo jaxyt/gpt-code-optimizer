63976a7296b4b82795daae95b1f19cf4f4aa9a9c
jaxyt <jax.maximillion.taylor@gmail.com>
2023-06-01 15:35:14 -0400

    got configurations working, still need 2 test mor

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,9 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { Configuration, OpenAIApi, CreateChatCompletionRequest } from "openai";
 
-const configuration = new Configuration({
-	apiKey: vscode.workspace.getConfiguration('gpt-code-optimizer').secretApiKey,
-});
+let configuration = null;
 
-const openai = new OpenAIApi(configuration);
+let openai = null;
 
 function getActiveEditorContent(): string | null {
 	const editor = vscode.window.activeTextEditor;
@@ -21,42 +17,31 @@ async function getOptimizedCode(code: string): Promise<string> {
 	  temperature: vscode.workspace.getConfiguration('gpt-code-optimizer').temperature,
 	  max_tokens: vscode.workspace.getConfiguration('gpt-code-optimizer').maxTokens
 	};
-  
 	const response = await openai.createChatCompletion(completionRequest);
-  
 	return response.data.choices[0].message.content.trim();
 }
 
 async function openNewTabWithOptimizedCode(optimizedCode: string) {
 	const doc = await vscode.workspace.openTextDocument({ language: vscode.window.activeTextEditor?.document.languageId, content: optimizedCode });
-	
 	await vscode.window.showTextDocument(doc, { preserveFocus: true, preview: true, viewColumn: vscode.ViewColumn.Beside });
 }
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	configuration = new Configuration({
+		apiKey: vscode.workspace.getConfiguration('gpt-code-optimizer').secretApiKey,
+	});
+	
+	openai = new OpenAIApi(configuration);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	// console.log('Congratulations, your extension "gpt-code-optimizer" is now active!');
-	// console.log(vscode.workspace.getConfiguration('gpt-code-optimizer'));
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('gpt-code-optimizer.optimizeCode', async () => {
 		const code = getActiveEditorContent();
-	
 		if (code) {
-			// const optimizedCode = await getOptimizedCode(code);
-			const optimizedCode = code;
+			const optimizedCode = await getOptimizedCode(code);
+			// const optimizedCode = code;
 			openNewTabWithOptimizedCode(optimizedCode);
 		}
 	});
-	
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
